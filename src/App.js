4b43d85c576cefc0371055c2d056a6ab386279cd
fixmylifedesigns <ijd.irving@gmail.com>
2019-11-19 17:19:00 -0500

    border

--- src/App.js
+++ src/App.js
@@ -3,20 +3,60 @@ import { Route, Switch } from "react-router-dom";
 import {
   Homepage,
   NotFound,
-  Footer,
+  Footer
 } from "./components/storeRoutes/storeRoutes";
+import cornerBorder from "./images/cornerborder.svg";
 function App() {
   return (
     <div className="App">
       {/* <Navigationbar /> */}
       <header className="App-header">
-
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="*" component={NotFound} />
         </Switch>
       </header>
-      <Footer />
+      <div style={{ position: "fixed", width: "100vw", height: "100vh" }}>
+        <img
+          src={cornerBorder}
+          style={{ width: "110px", left: "0%", top: "0%", position: "fixed" }}
+        />
+        <img
+          src={cornerBorder}
+          style={{
+            width: "110px",
+            left: "0%",
+            bottom: "0%",
+            position: "fixed",
+            transform: "rotate(270deg)"
+          }}
+        />
+        <img
+          src={cornerBorder}
+          style={{
+            width: "110px",
+            right: "0%",
+            bottom: "0%",
+            // float: "right",
+            position: "fixed",
+            transform: "rotate(180deg)"
+          }}
+        />
+        <img
+          src={cornerBorder}
+          style={{
+            right: "100%",
+            top: "0%",
+            // width: "100px",
+            width: "110px",
+            right: "0%",
+            float: "right",
+            position: "fixed",
+            transform: "rotate(90deg)"
+          }}
+        />
+      </div>
+      <Footer />{" "}
     </div>
   );
 }
--- src/components/navigation/footer.js
+++ src/components/navigation/footer.js
@@ -5,7 +5,8 @@ export default class Footer extends Component {
     render() {
         return (
             <div className="footer-container">
-                <h4>Copyright © Design by <a href="https://www.duranirving.com" style={{textDecoration:"none", color:"#E6AB1C"}}>Irving Duran</a> 2019 </h4>
+                <h4>Copyright © 2019 </h4>
+                <h4>Design by <a href="https://www.duranirving.com" style={{textDecoration:"none", color:"#E6AB1C"}}>Irving Duran</a> </h4>
             </div>
         )
     }
