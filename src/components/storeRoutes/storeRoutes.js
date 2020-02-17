0e1a60a1460cbc5228b797c51e5b59625e8c19b4
fixmylifedesigns <ijd.irving@gmail.com>
2020-02-17 17:42:05 -0500

    menu button change

--- src/components/storeRoutes/storeRoutes.js
+++ src/components/storeRoutes/storeRoutes.js
@@ -1,6 +1,5 @@
 import Homepage from "../routes/home";
 import NotFound from "../routes/notFound";
 import Footer from "../navigation/footer";
-import Navigationbar from "../navigation/navigationbar";
 
-export { Homepage, NotFound, Footer, Navigationbar };
+export { Homepage, NotFound, Footer};
